34390865cca325896e275ad38790d8c63b2bf898
lm21lawo <lm21lawo@studserv.uni-leipzig.de>
2025-10-30 15:45:55 +0100

    created simple Calculator with 2 Ints

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("geben SIe eine Zahl ein");
+        Scanner s = new Scanner(System.in);
+        int zahl1 = s.nextInt();
+        System.out.println("Zahl 2");
+        int zahl2 = s.nextInt();
+        s.close();
+
+        System.out.println("Ergebnisse: ");
+        System.out.println("Summe: " + (zahl1 + zahl2));
+        System.out.println("Summe: " + (zahl1 - zahl2));
+        System.out.println("Summe: " + (zahl1 * zahl2));
+        System.out.println("Summe: " + (zahl1 / zahl2));
+
     }
 }
